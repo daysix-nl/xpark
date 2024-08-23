d816f2e177007f8665ef76e197d9fc9e8093e0f2
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-08-23 12:39:32 +0200

    wip

--- blocks/acf-header-2/acf-header-2.php
+++ blocks/acf-header-2/acf-header-2.php
@@ -8,7 +8,7 @@ else: ?>
    <div class="w-[360px] md:w-[517px] lg:w-[682px] xl:w-[800px] mx-auto text-center">
       <h1 class="text-14 leading-14 md:text-16 md:leading-16 lg:text-16 lg:leading-16 xl:text-18 xl:leading-18 tracking-[0.01em] font-semibold uppercase text-[#FF6441]"><?php the_title();?></h1>
       <h2 class="text-25 leading-35 md:text-30 md:leading-40 lg:text-26 lg:leading-36 xl:text-30 xl:leading-40 font-bold mt-[10px]"><?php echo get_field('subtitel');?><span class="text-[#FF6441]">.</span></h2>
-      <p class="text-15 leading-28 md:text-17 md:leading-30 lg:text-15 lg:leading-28 xl:text-17 xl:leading-30 mt-[10px]"><?php echo get_field('tekst');?></p>
+      <div class="text-15 leading-28 md:text-17 md:leading-30 lg:text-15 lg:leading-28 xl:text-17 xl:leading-30 mt-[10px] text-editor inside"><?php echo get_field('tekst');?></div>
    </div>
  </section>
 
